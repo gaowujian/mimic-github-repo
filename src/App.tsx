a373074f0650c7ea403f60065ad79788310296f8
tony <gwjlshebei@gmail.com>
2022-01-04 17:30:32 +0800

    完成一半的右侧栏

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ import {
   Typography,
 } from "antd";
 
-import { NotificationOutlined, EditOutlined } from "@ant-design/icons";
+import { NotificationOutlined, EditOutlined, CloseCircleFilled, CloseCircleOutlined } from "@ant-design/icons";
 const { Text } = Typography;
 const Content: React.FC<Record<string, any>> = function (props) {
   const { children, ...rest } = props;
@@ -101,19 +101,60 @@ function App() {
           <li>avatar支持多种形式, 包括src属性添加图片形式，和icon属性添加组件形式</li>
           <li>dropdown(下拉菜单)一般直接配合menu，不用搭配card, List等组件,可能会造成card的自带样式失效</li>
           <li>用于数据展示的列表可以用List通用列表,用于地址跳转的可以用Menu，菜单列表</li>
+          <li>按钮点击后出下拉菜单:1: 使用 dropdown 包裹自定义的button样式 2. dropdown.button有相对固定的api</li>
         </ul>
       </PageHeader>
       <Content className="app-content">
-        <Breadcrumb separator="/">
-          <Breadcrumb.Item>
-            <Text className="text">gsoft-inc</Text>
-          </Breadcrumb.Item>
-          <Breadcrumb.Item>
-            <Text className="text" strong>
-              craco
-            </Text>
-          </Breadcrumb.Item>
-        </Breadcrumb>
+        <Row>
+          <Breadcrumb separator="/">
+            <Breadcrumb.Item>
+              <Text className="text">gsoft-inc</Text>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Text className="text" strong>
+                craco
+              </Text>
+            </Breadcrumb.Item>
+          </Breadcrumb>
+          <Space style={{ marginLeft: "auto" }}>
+            <Dropdown
+              placement="bottomLeft"
+              overlay={() => {
+                return (
+                  // <Menu>
+                  //   <Menu.Item>Menu</Menu.Item>
+                  // </Menu>
+                  <Card extra={<CloseCircleOutlined />}>xx</Card>
+                );
+              }}
+            >
+              <Button>
+                <Space align="center">
+                  <span className="iconfont icon-eye"></span>
+                  <span>Watch</span>
+                  <span className="iconfont icon-caretdown"></span>
+                </Space>
+              </Button>
+            </Dropdown>
+            <Button>
+              <Space align="center">
+                <span className="iconfont icon-fork"></span>
+                <span>Fork</span>
+              </Space>
+            </Button>
+            <Dropdown.Button
+              icon={<span className="iconfont icon-caretdown"></span>}
+              overlay={() => {
+                return <></>;
+              }}
+            >
+              <Space align="center">
+                <span className="iconfont icon-star"></span>
+                <span>Starred</span>
+              </Space>
+            </Dropdown.Button>
+          </Space>
+        </Row>
       </Content>
     </div>
   );
