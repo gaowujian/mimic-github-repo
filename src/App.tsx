581733f5eacf24b1fa1792a26859d879bd3a0c50
tony <gwjlshebei@gmail.com>
2022-01-05 17:30:57 +0800

    实现watch的弹出框

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
+import type { ReactText } from "react";
 import "./App.css";
 import ReactDOM from "react-dom";
+import ProList from "@ant-design/pro-list";
 
 import {
   PageHeader,
@@ -16,20 +18,32 @@ import {
   Input,
   Breadcrumb,
   Typography,
+  Tabs,
 } from "antd";
 
 import { NotificationOutlined, EditOutlined, CloseCircleFilled, CloseCircleOutlined } from "@ant-design/icons";
+import { useReducer } from "react";
 const { Text } = Typography;
-const Content: React.FC<Record<string, any>> = function (props) {
+
+const SimpleCard: React.FC<Record<string, any>> = function (props) {
   const { children, ...rest } = props;
-  return <div {...rest}>{children}</div>;
+  return <div style={{ border: "1px solid #ddd", borderRadius: "5px" }}>{children}</div>;
 };
+
+const data = [{ desc: "participating and mentions" }, { desc: "all activity" }, { desc: "ignore" }, { desc: "custom" }];
 
 function App() {
   const [inputExpanded, setInputExpanded] = useState(false);
   const baseStyle = { transition: "all 0.1s ease-in-out" };
   const inputStyle = (inputExpanded && { ...baseStyle, width: "400px" }) || baseStyle;
   const inputSuffix = !inputExpanded ? <EditOutlined /> : <div></div>;
+  const [selectedRowKeys, setSelectedRowKeys] = useState<ReactText[]>([]);
+  const [watchVisible, setWatchVisible] = useState(false);
+  const [, forceUpdate] = useReducer((count) => count + 1, { count: 0 });
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: (keys: ReactText[]) => setSelectedRowKeys(keys),
+  };
   return (
     <div>
       <PageHeader
@@ -100,11 +114,22 @@ function App() {
           <li>使用阿里巴巴iconfont图标库</li>
           <li>avatar支持多种形式, 包括src属性添加图片形式，和icon属性添加组件形式</li>
           <li>dropdown(下拉菜单)一般直接配合menu，不用搭配card, List等组件,可能会造成card的自带样式失效</li>
+          <li>dropdown内部如果样式失效，使用div包括组件，例如div包裹一个card</li>
           <li>用于数据展示的列表可以用List通用列表,用于地址跳转的可以用Menu，菜单列表</li>
           <li>按钮点击后出下拉菜单:1: 使用 dropdown 包裹自定义的button样式 2. dropdown.button有相对固定的api</li>
+          <li>使用了prolist组件，基于protable但是赋与了list更多table的功能，包括选中，展开等</li>
+          <li>prolist对每一行的数据有较为明确的定义，也就是columns较为固定,title,subtitle,description</li>
+          <li>使用card作为容器，主要利用了他可以添加padding，以及size调整padding大小，并快速添加边框，</li>
+          <li>有的时候只需要简单的padding，也可以直接添加一个css规则 app.style,避免可能引入多余的不满意样式</li>
+          <li>
+            为了实现在dropdown的弹出层区域内点击一个关闭按钮，让dropdown消失，所以我们需要给dropdown添加visible属性
+          </li>
+          <li>
+            点击其他区域触发一些行为的等价效果就是，当前区域失去了焦点，对于button来说，点击弹出下拉菜单，设置button的blur事件，也就是按钮失去焦点，会导致菜单收回
+          </li>
         </ul>
       </PageHeader>
-      <Content className="app-content">
+      <div className="app-content">
         <Row>
           <Breadcrumb separator="/">
             <Breadcrumb.Item>
@@ -118,17 +143,59 @@ function App() {
           </Breadcrumb>
           <Space style={{ marginLeft: "auto" }}>
             <Dropdown
-              placement="bottomLeft"
+              placement="bottomRight"
+              trigger={["click"]}
+              overlayStyle={{
+                width: "300px",
+              }}
+              visible={watchVisible}
               overlay={() => {
                 return (
-                  // <Menu>
-                  //   <Menu.Item>Menu</Menu.Item>
-                  // </Menu>
-                  <Card extra={<CloseCircleOutlined />}>xx</Card>
+                  <SimpleCard>
+                    <ProList
+                      className="watch-card"
+                      toolBarRender={() => {
+                        return [
+                          <CloseCircleOutlined
+                            style={{ fontSize: "17px" }}
+                            onClick={() => {
+                              setWatchVisible(false);
+                            }}
+                          />,
+                        ];
+                      }}
+                      split
+                      onRow={(record: any) => {
+                        return {
+                          onClick: () => {
+                            console.log(record);
+                          },
+                        };
+                      }}
+                      headerTitle={<div>Notification:{selectedRowKeys}</div>}
+                      rowKey={(row) => {
+                        return row.desc;
+                      }}
+                      dataSource={data}
+                      metas={{
+                        description: {
+                          dataIndex: "desc",
+                        },
+                      }}
+                      rowSelection={rowSelection}
+                    ></ProList>
+                  </SimpleCard>
                 );
               }}
             >
-              <Button>
+              <Button
+                onClick={() => {
+                  setWatchVisible(!watchVisible);
+                }}
+                onBlur={() => {
+                  setWatchVisible(false);
+                }}
+              >
                 <Space align="center">
                   <span className="iconfont icon-eye"></span>
                   <span>Watch</span>
@@ -155,7 +222,27 @@ function App() {
             </Dropdown.Button>
           </Space>
         </Row>
-      </Content>
+        {/* <Tabs defaultActiveKey="1" type="line" style={{ height: "500px" }}>
+          <Tabs.TabPane tab="Tab 1" key="1">
+            Content of Tab Pane 1
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Tab 2" key="2">
+            Content of Tab Pane 2
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Tab 3" key="3">
+            Content of Tab Pane 3
+          </Tabs.TabPane>
+        </Tabs>
+        <List
+          header={<div>Header</div>}
+          footer={<div>Footer</div>}
+          bordered
+          dataSource={data}
+          renderItem={(item) => <List.Item>{item}</List.Item>}
+          itemLayout="horizontal"
+        /> */}
+        <div style={{ height: "400px" }}></div>
+      </div>
     </div>
   );
 }
